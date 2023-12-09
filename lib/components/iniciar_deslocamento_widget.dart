084200f263b1eaad644f1febbda1133ddec2801a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-09 16:43:57 +0000

    Updating to latest FlutterFlow output.

--- lib/components/iniciar_deslocamento_widget.dart
+++ lib/components/iniciar_deslocamento_widget.dart
@@ -1,4 +1,5 @@
 import '/backend/api_requests/api_calls.dart';
+import '/components/loading_comp_widget.dart';
 import '/components/motivo_pausa_widget.dart';
 import '/flutter_flow/flutter_flow_google_map.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
@@ -512,7 +513,24 @@ class _IniciarDeslocamentoWidgetState extends State<IniciarDeslocamentoWidget> {
                                     16.0, 16.0, 16.0, 16.0),
                                 child: FFButtonWidget(
                                   onPressed: () async {
-                                    FFAppState().update(() {});
+                                    _model.updatePage(() {});
+                                    await showModalBottomSheet(
+                                      isScrollControlled: true,
+                                      backgroundColor: Colors.transparent,
+                                      enableDrag: false,
+                                      context: context,
+                                      builder: (context) {
+                                        return Padding(
+                                          padding:
+                                              MediaQuery.viewInsetsOf(context),
+                                          child: LoadingCompWidget(),
+                                        );
+                                      },
+                                    ).then((value) => safeSetState(() {}));
+
+                                    await Future.delayed(
+                                        const Duration(milliseconds: 1000));
+                                    Navigator.pop(context);
                                   },
                                   text: '',
                                   icon: Icon(
