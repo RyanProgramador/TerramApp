e62b726b5440c4997bb4049927fb36c8fd320962
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-11 18:21:08 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/contorno_map.dart
+++ lib/custom_code/widgets/contorno_map.dart
@@ -34,14 +34,19 @@ class _ContornoMapState extends State<ContornoMap> {
   List<google_maps.Polygon> polygons = [];
   bool isLocationPaused = false;
 
+  double currentZoom = 19.0;
+  LatLng? currentTarget;
+
   void _onMapCreated(google_maps.GoogleMapController controller) {
     _googleMapController = controller;
+    _getCurrentLocation(); // Inicializar a posição atual ao criar o mapa
   }
 
   void _getCurrentLocation() async {
     if (!isLocationPaused) {
       Position newLoc = await Geolocator.getCurrentPosition();
 
+      double currentZoomLevel = await _googleMapController!.getZoomLevel();
       if (_googleMapController != null) {
         _googleMapController!.animateCamera(
           google_maps.CameraUpdate.newCameraPosition(
@@ -50,11 +55,13 @@ class _ContornoMapState extends State<ContornoMap> {
                 newLoc.latitude,
                 newLoc.longitude,
               ),
-              zoom: 19,
+              zoom: currentZoomLevel,
             ),
           ),
         );
       }
+      currentTarget = LatLng(newLoc.latitude, newLoc.longitude);
+      currentZoom = 19; // Atualizar o zoom padrão
 
       setState(() {
         position = newLoc;
@@ -68,6 +75,12 @@ class _ContornoMapState extends State<ContornoMap> {
             icon: google_maps.BitmapDescriptor.defaultMarkerWithHue(
               google_maps.BitmapDescriptor.hueBlue,
             ),
+            visible: true,
+            draggable: false,
+            infoWindow: google_maps.InfoWindow(
+              title:
+                  'UserMarkerID${markers.length}', // Define a posição do pop-up para o centro do marcador
+            ),
           ),
         );
         _updatePolygon();
@@ -75,12 +88,23 @@ class _ContornoMapState extends State<ContornoMap> {
     }
   }
 
-  void _onMarkerDragEnd(google_maps.LatLng position) {
-    // Handle marker drag end
-    print("Marker Drag End: $position");
-
-    _updatePolygon();
+  void _centerMap() {
+    if (currentTarget != null && _googleMapController != null) {
+      _googleMapController!.animateCamera(
+        google_maps.CameraUpdate.newLatLngZoom(
+          google_maps.LatLng(currentTarget!.latitude, currentTarget!.longitude),
+          currentZoom,
+        ),
+      );
+    }
   }
+
+  // void _onMarkerDragEnd(google_maps.LatLng position) {
+  //   // Handle marker drag end
+  //   print("Marker Drag End: $position");
+  //
+  //   //_updatePolygon();
+  // }
 
   void _updatePolygon() {
     setState(() {
@@ -92,7 +116,7 @@ class _ContornoMapState extends State<ContornoMap> {
         var polygon = google_maps.Polygon(
           polygonId: google_maps.PolygonId('AreaPolygon'),
           points: polygonCoordinates,
-          fillColor: Colors.blue.withOpacity(0.0),
+          fillColor: Colors.blue.withOpacity(0.2),
           strokeColor: Colors.blue,
           strokeWidth: 6,
         );
@@ -101,16 +125,16 @@ class _ContornoMapState extends State<ContornoMap> {
     });
   }
 
-  void _pauseLocation() {
+  void _toggleLocationPause() {
     setState(() {
-      isLocationPaused = true;
+      isLocationPaused = !isLocationPaused;
     });
   }
 
-  void _finishArea() {
+  void _clearMarkersAndPolygons() {
     setState(() {
-      isLocationPaused = true;
-      _updatePolygon();
+      markers.clear();
+      polygons.clear();
     });
   }
 
@@ -140,22 +164,70 @@ class _ContornoMapState extends State<ContornoMap> {
             onMapCreated: _onMapCreated,
             markers: {...markers}.toSet(),
             polygons: {...polygons}.toSet(),
+            mapType: google_maps.MapType.satellite,
           ),
         ),
         Positioned(
           top: 16,
           right: 16,
           child: ElevatedButton(
-            onPressed: _pauseLocation,
-            child: Text("Pause Location"),
+            onPressed: _toggleLocationPause,
+            style: ElevatedButton.styleFrom(
+              shape: CircleBorder(),
+              backgroundColor: Color(0xFF00736D),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: isLocationPaused
+                  ? Icon(
+                      Icons.play_arrow,
+                      size: 25.0,
+                      color: Colors.white,
+                    )
+                  : Icon(
+                      Icons.pause,
+                      size: 25.0,
+                      color: Colors.white,
+                    ),
+            ),
           ),
         ),
         Positioned(
           top: 16,
-          right: 120,
+          left: MediaQuery.of(context).size.width / 2 - 44,
           child: ElevatedButton(
-            onPressed: _finishArea,
-            child: Text("Finish Area"),
+            style: ElevatedButton.styleFrom(
+              shape: CircleBorder(),
+              backgroundColor: Color(0xFF00736D),
+            ),
+            onPressed: _centerMap,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.center_focus_strong,
+                size: 25.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 16,
+          left: 16,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: CircleBorder(),
+              backgroundColor: Color(0xFF00736D),
+            ),
+            onPressed: _clearMarkersAndPolygons,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.delete,
+                size: 25.0,
+                color: Colors.white,
+              ),
+            ),
           ),
         ),
       ],
