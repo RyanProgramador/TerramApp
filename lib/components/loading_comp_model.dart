c29358c11294c339effa88bcf42f735913e37088
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-14 11:21:52 +0000

    Updating to latest FlutterFlow output.

--- lib/components/loading_comp_model.dart
+++ lib/components/loading_comp_model.dart
@@ -1,17 +1,39 @@
 import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_timer.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/widgets/index.dart' as custom_widgets;
 import 'loading_comp_widget.dart' show LoadingCompWidget;
+import 'package:stop_watch_timer/stop_watch_timer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class LoadingCompModel extends FlutterFlowModel<LoadingCompWidget> {
+  ///  Local state fields for this component.
+
+  bool mostraNaTela = false;
+
+  ///  State fields for stateful widgets in this component.
+
+  // State field(s) for Timer widget.
+  int timerMilliseconds = 25000;
+  String timerValue = StopWatchTimer.getDisplayTime(
+    25000,
+    hours: false,
+    milliSecond: false,
+  );
+  FlutterFlowTimerController timerController =
+      FlutterFlowTimerController(StopWatchTimer(mode: StopWatchMode.countDown));
+
   /// Initialization and disposal methods.
 
   void initState(BuildContext context) {}
 
-  void dispose() {}
+  void dispose() {
+    timerController.dispose();
+  }
 
   /// Action blocks are added here.
 
--- lib/components/loading_comp_widget.dart
+++ lib/components/loading_comp_widget.dart
@@ -1,6 +1,10 @@
 import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_timer.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/widgets/index.dart' as custom_widgets;
+import 'package:stop_watch_timer/stop_watch_timer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -27,6 +31,13 @@ class _LoadingCompWidgetState extends State<LoadingCompWidget> {
   void initState() {
     super.initState();
     _model = createModel(context, () => LoadingCompModel());
+
+    // On component load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      _model.timerController.onStartTimer();
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
   }
 
   @override
@@ -46,41 +57,135 @@ class _LoadingCompWidgetState extends State<LoadingCompWidget> {
       decoration: BoxDecoration(
         color: Color(0x00F1F4F8),
       ),
-      child: Padding(
-        padding: EdgeInsetsDirectional.fromSTEB(16.0, 24.0, 16.0, 24.0),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Carregando...',
-                    textAlign: TextAlign.start,
-                    style: FlutterFlowTheme.of(context).headlineLarge.override(
-                          fontFamily: 'Outfit',
-                          color: Colors.black,
-                          fontWeight: FontWeight.w600,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(16.0, 24.0, 16.0, 24.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (!_model.mostraNaTela)
+                  Expanded(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 140.0,
+                          height: 140.0,
+                          child: custom_widgets.LoadingCircle(
+                            width: 140.0,
+                            height: 140.0,
+                            color: FlutterFlowTheme.of(context).primary,
+                            circleRadius: 30.0,
+                          ),
                         ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(
+                              0.0, 22.0, 0.0, 0.0),
+                          child: Text(
+                            'Por favor, aguarde.',
+                            style: FlutterFlowTheme.of(context)
+                                .headlineLarge
+                                .override(
+                                  fontFamily: 'Readex Pro',
+                                  color: Colors.black,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  Text(
-                    'Por favor, aguarde.',
-                    style: FlutterFlowTheme.of(context).headlineLarge.override(
-                          fontFamily: 'Readex Pro',
-                          color: Colors.black,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w300,
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(16.0, 24.0, 16.0, 24.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (_model.mostraNaTela)
+                  Expanded(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(
+                              0.0, 0.0, 0.0, 22.0),
+                          child: Text(
+                            'Parece que não ha serviços para você!',
+                            style: FlutterFlowTheme.of(context)
+                                .headlineLarge
+                                .override(
+                                  fontFamily: 'Readex Pro',
+                                  color: Colors.black,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                          ),
                         ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(
+                              0.0, 0.0, 0.0, 22.0),
+                          child: Text(
+                            'Tente novamente mais tarde.',
+                            style: FlutterFlowTheme.of(context)
+                                .headlineLarge
+                                .override(
+                                  fontFamily: 'Readex Pro',
+                                  color: Colors.black,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+              ],
+            ),
+          ),
+          if (true == false)
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(16.0, 24.0, 16.0, 24.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FlutterFlowTimer(
+                    initialTime: _model.timerMilliseconds,
+                    getDisplayTime: (value) => StopWatchTimer.getDisplayTime(
+                      value,
+                      hours: false,
+                      milliSecond: false,
+                    ),
+                    controller: _model.timerController,
+                    updateStateInterval: Duration(milliseconds: 1000),
+                    onChanged: (value, displayTime, shouldUpdate) {
+                      _model.timerMilliseconds = value;
+                      _model.timerValue = displayTime;
+                      if (shouldUpdate) setState(() {});
+                    },
+                    onEnded: () async {
+                      setState(() {
+                        _model.mostraNaTela = true;
+                      });
+                    },
+                    textAlign: TextAlign.start,
+                    style: FlutterFlowTheme.of(context).headlineSmall,
                   ),
                 ],
               ),
             ),
-          ],
-        ),
+        ],
       ),
     );
   }
