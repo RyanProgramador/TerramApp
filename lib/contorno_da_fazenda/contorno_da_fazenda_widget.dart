d6644e25db1baae45104bf0320183673e31b19e1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-18 15:49:04 +0000

    Updating to latest FlutterFlow output.

--- lib/contorno_da_fazenda/contorno_da_fazenda_widget.dart
+++ lib/contorno_da_fazenda/contorno_da_fazenda_widget.dart
@@ -159,30 +159,6 @@ class _ContornoDaFazendaWidgetState extends State<ContornoDaFazendaWidget> {
                                   .secondaryBackground,
                             ),
                             alignment: AlignmentDirectional(0.0, 1.0),
-                            child: Container(
-                              width: double.infinity,
-                              height: MediaQuery.sizeOf(context).height * 1.0,
-                              child: custom_widgets.ContornoMapCorte(
-                                width: double.infinity,
-                                height: MediaQuery.sizeOf(context).height * 1.0,
-                                ativoOuNao: _model.ativo,
-                                oservid: valueOrDefault<String>(
-                                  widget.oservID,
-                                  '1',
-                                ),
-                                idContorno: widget.idDoContorno,
-                                fazNome: widget.fazendaNome,
-                                toleranciaEmMetrosEntreUmaCapturaEOutra: FFAppState()
-                                            .distanciaEmMetrosDeToleranciaEntreUmaCapturaEOutra !=
-                                        null
-                                    ? FFAppState()
-                                        .distanciaEmMetrosDeToleranciaEntreUmaCapturaEOutra
-                                    : 1,
-                                fazid: widget.fazid!,
-                                localAtual: currentUserLocationValue,
-                                fazLatLng: widget.fazlatlng,
-                              ),
-                            ),
                           ),
                         ),
                       ),
