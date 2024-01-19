639383199f9a00068eb8ac033d17cfd8d2095678
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-19 18:01:18 +0000

    Updating to latest FlutterFlow output.

--- lib/medicao_coleta/medicao_coleta_widget.dart
+++ lib/medicao_coleta/medicao_coleta_widget.dart
@@ -156,23 +156,6 @@ class _MedicaoColetaWidgetState extends State<MedicaoColetaWidget> {
                       child: FFButtonWidget(
                         onPressed: () async {
                           FFAppState().update(() {});
-
-                          context.goNamed(
-                            'MedicaoColeta',
-                            queryParameters: {
-                              'fazNome': serializeParam(
-                                widget.fazNome,
-                                ParamType.String,
-                              ),
-                            }.withoutNulls,
-                            extra: <String, dynamic>{
-                              kTransitionInfoKey: TransitionInfo(
-                                hasTransition: true,
-                                transitionType: PageTransitionType.fade,
-                                duration: Duration(milliseconds: 0),
-                              ),
-                            },
-                          );
                         },
                         text: '',
                         icon: Icon(
