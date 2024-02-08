18828dd620248dd1340af07bdf48967048eb9940
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-08 16:51:14 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/deseja_realmente_voltar.dart
+++ lib/custom_code/widgets/deseja_realmente_voltar.dart
@@ -31,8 +31,8 @@ class _DesejaRealmenteVoltarState extends State<DesejaRealmenteVoltar> {
         final shouldPop = await showDialog(
           context: context,
           builder: (context) => AlertDialog(
-            title: Text(''),
-            content: Text('Você quer sair do serviço e voltar à tela inicial?'),
+            title: Text('Você quer sair do serviço e voltar à tela inicial?'),
+            content: Text(''),
             actions: <Widget>[
               TextButton(
                 onPressed: () => Navigator.of(context)
