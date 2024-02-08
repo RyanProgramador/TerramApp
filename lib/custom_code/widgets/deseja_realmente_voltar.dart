5b1575bdfeed5f338af3d3fd226d01bf73287dbe
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-08 13:51:36 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/deseja_realmente_voltar.dart
+++ lib/custom_code/widgets/deseja_realmente_voltar.dart
@@ -9,38 +9,54 @@ import 'package:flutter/material.dart';
 // Begin custom widget code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
-@override
-Widget build(BuildContext context) {
-  return WillPopScope(
-    onWillPop: () async {
-      final shouldPop = await showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Tem certeza?'),
-          content: Text('Você quer sair desta tela?'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () =>
-                  Navigator.of(context).pop(false), // Não permite sair da tela
-              child: Text('Não'),
-            ),
-            TextButton(
-              onPressed: () =>
-                  Navigator.of(context).pop(true), // Permite sair da tela
-              child: Text('Sim'),
-            ),
-          ],
-        ),
-      );
+class DesejaRealmenteVoltar extends StatefulWidget {
+  const DesejaRealmenteVoltar({
+    super.key,
+    this.width,
+    this.height,
+  });
 
-      return shouldPop ??
-          false; // Retorna true se o usuário escolheu 'Sim', caso contrário, false
-    },
-    child: Scaffold(
-      appBar: AppBar(
-        title: Text("Tela com WillPopScope"),
+  final double? width;
+  final double? height;
+
+  @override
+  State<DesejaRealmenteVoltar> createState() => _DesejaRealmenteVoltarState();
+}
+
+class _DesejaRealmenteVoltarState extends State<DesejaRealmenteVoltar> {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        final shouldPop = await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Tem certeza?'),
+            content: Text('Você quer sair desta tela?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context)
+                    .pop(false), // Não permite sair da tela
+                child: Text('Não'),
+              ),
+              TextButton(
+                onPressed: () =>
+                    Navigator.of(context).pop(true), // Permite sair da tela
+                child: Text('Sim'),
+              ),
+            ],
+          ),
+        );
+
+        return shouldPop ??
+            false; // Retorna true se o usuário escolheu 'Sim', caso contrário, false
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Tela com WillPopScope"),
+        ),
+        // O restante da estrutura da sua tela aqui
       ),
-      // O restante da estrutura da sua tela aqui
-    ),
-  );
+    );
+  }
 }
