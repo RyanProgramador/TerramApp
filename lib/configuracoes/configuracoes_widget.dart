acc39833d9f0aeb84af6f2dbce63799a4ecd4338
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-17 01:29:17 +0000

    Updating to latest FlutterFlow output.

--- lib/configuracoes/configuracoes_widget.dart
+++ lib/configuracoes/configuracoes_widget.dart
@@ -772,7 +772,7 @@ class _ConfiguracoesWidgetState extends State<ConfiguracoesWidget> {
                                   padding: EdgeInsetsDirectional.fromSTEB(
                                       0.0, 4.0, 0.0, 0.0),
                                   child: Text(
-                                    'v1.9.529',
+                                    'v1.9.530',
                                     style: FlutterFlowTheme.of(context)
                                         .labelMedium
                                         .override(
